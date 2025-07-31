d9d08523391aa3dc9c9f393c8bedb533a1066203
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-01 00:30:36 +0600

    fixed

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -8,12 +8,12 @@ const Navbar = (props: Props) => {
     <div className="_container pt-6 pb-8">
       <div className="flex justify-between items-center bg-[#1F2535] px-3 py-1 rounded-2xl">
         <div className="relative min-h-[60px] min-w-[200px]">
-          {/* <Image
+          <Image
             src="/images/logo.svg"
             alt="logo"
             fill
             className="object-contain brightness-0 invert"
-          /> */}
+          />
         </div>
         <div>
           <Button variant="primary" className="rounded-xl h-[43px]">
