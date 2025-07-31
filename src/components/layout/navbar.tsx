f5e64617870341a4f461698a3de6ed73715dfdbe
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-31 22:38:26 +0600

    test

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className='_container'>
+        <p>test</p>
+    </div>
+  )
+}
+
+export default Navbar
